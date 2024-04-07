ebc668af0ccc80aeec3aea100cd88dd448cc00d5
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-04-07 05:07:52 -0600

    Messed around with some formatting

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-    <router-link to="/">Home</router-link>
-    <router-link :to="{ name: 'McStatus' }">MC Status</router-link>
-    <router-view></router-view>
+    <div class="header">
+        <router-link to="/">NeuralNexus</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/contact">Contact</router-link>
+        <router-link to="/projects">Projects</router-link>
+        <router-link class="login-button" to="/login">Login</router-link>
+    </div>
+
+    <div id="app" class="app">
+        <br />
+
+        <router-link :to="{ name: 'McStatus' }">MC Status</router-link>
+        <router-view></router-view>
+    </div>
 </template>
