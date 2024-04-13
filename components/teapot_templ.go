ab1cfa5bfdc935707299831c830290303b6a0411
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-04-12 19:22:47 -0600

    Improved the teapot

--- components/teapot_templ.go
+++ components/teapot_templ.go
@@ -34,6 +34,24 @@ func TeapotPage() templ.Component {
 	})
 }
 
+func logTeapotRequest() templ.ComponentScript {
+	return templ.ComponentScript{
+		Name: `__templ_logTeapotRequest_fcfc`,
+		Function: `function __templ_logTeapotRequest_fcfc(){fetch('https://api.neuralnexus.dev/api/v1/teapot')
+		.then((response) => {
+			if (response.status !== 418) {
+				console.error('Failed to log teapot request:', response.statusText);
+			}
+		})
+		.catch((error) => {
+			console.error('Failed to log teapot request:', error);
+		});
+}`,
+		Call:       templ.SafeScript(`__templ_logTeapotRequest_fcfc`),
+		CallInline: templ.SafeScriptInline(`__templ_logTeapotRequest_fcfc`),
+	}
+}
+
 func Teapot() templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
@@ -47,6 +65,10 @@ func Teapot() templ.Component {
 			templ_7745c5c3_Var2 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
+		templ_7745c5c3_Err = logTeapotRequest().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"flex flex-col items-center justify-center gap-4 text-center\"><img src=\"/public/teapot.jpg\" width=\"200\" height=\"200\" alt=\"Teapot\" class=\"rounded-full border-4 border-white\" style=\"aspect-ratio: 200 / 200; object-fit: cover;\"><div class=\"space-y-2\"><h1 class=\"text-4xl font-bold\">418 I'm a teapot</h1><p class=\"text-sm text-gray-500\">You requested a cup of coffee, but I'm a teapot.</p></div><a class=\"rounded-full w-full max-w-xs\" href=\"/\">Brew another cup</a></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
